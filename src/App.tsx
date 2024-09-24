5a97b41c647b7ad1f9a9a6a90578ba76ccbfd418
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-09-24 01:03:00 -0300

    refactor: use "any" for more flexibility in code

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import searchImages from "./api"
-import SearchBar from "./components/SearchBar"
+import searchImages from "./api";
+import SearchBar from "./components/SearchBar";
 
-function App():JSX.Element{
-  const handleSubmit = (searchTerm:string):void|string => {
-    alert("Do a search with " + searchTerm)
-  }
-  
-  return(
-  <div>
-    <SearchBar onSubmit = {handleSubmit} />
-  </div>
-  ) 
+function App(): JSX.Element {
+  const handleSubmit = (searchTerm: any) => {
+    alert("Do a search with " + searchTerm);
+  };
+
+  return (
+    <div>
+      <SearchBar onSubmit={handleSubmit} />
+    </div>
+  );
 }
 
-export default App
+export default App;
